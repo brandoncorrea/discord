7a8822630d16f5a8fed17aac4e79b74b2e4e6798
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-09-14 12:45:53 -0400

    Removed print statements

--- dicaprio/bot.py
+++ dicaprio/bot.py
@@ -18,16 +18,12 @@ async def on_ready():
   print(f'{client.user} has connected to Discord!')
 
 def has_joined(before, after):
-  print(f'before status: {before.status}')
-  print(f'after status: {after.status}')
   return before.status == discord.Status.offline and after.status != discord.Status.offline
 
 def can_send_message(channel):
-  print(f'messaging permission? {channel.permissions_for(channel.guild.me).send_messages}')
   return channel.permissions_for(channel.guild.me).send_messages
 
 async def greet_member(member, channel):
-  print('maybe sending a greeting!')
   if member.name == 'ᴰᵉᵘᶜᵉˢ' and member.discriminator == '9928':
     await channel.send(f'{member.mention} Hey Daddy')
   elif member.display_name == 'ᴰᵉᵘᶜᵉˢ' and member.discriminator == '1066':
@@ -36,9 +32,7 @@ async def greet_member(member, channel):
 @client.event
 async def on_presence_update(before, after):
   if has_joined(before, after):
-    print(f'{after.name} has joined!')
     for channel in after.guild.text_channels:
-      print(f'Checking channel: {channel}')
       if can_send_message(channel):
         await greet_member(after, channel)
 
