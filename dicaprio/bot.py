2cd43412949a5035299b747271e886cc59f2a98d
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-09-14 10:55:18 -0400

    Add return statements

--- dicaprio/bot.py
+++ dicaprio/bot.py
@@ -17,10 +17,10 @@ async def on_ready():
   print(f'{client.user} has connected to Discord!')
 
 def has_joined(before, after):
-  before.status == discord.Status.offline and after.status != discord.Status.offline
+  return before.status == discord.Status.offline and after.status != discord.Status.offline
 
 def can_send_message(channel):
-  channel.permissions_for(channel.guild.me).send_messages
+  return channel.permissions_for(channel.guild.me).send_messages
 
 @client.event
 async def on_presence_update(before, after):
