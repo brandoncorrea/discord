0cf8efc608cf452b734a5d3f80c5e399db7d0ee5
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-09-15 21:46:28 -0400

    Add tests for and refactor the bot

--- dicaprio/bot.py
+++ dicaprio/bot.py
@@ -18,54 +18,6 @@ client = discord.Client(intents=intents)
 async def on_ready():
   print(f'{client.user} has connected to Discord!')
 
-def has_joined(before, after):
-  return before.status == discord.Status.offline and after.status != discord.Status.offline
-
-def can_send_message(member, channel):
-  return channel.permissions_for(member).send_messages
-
-def random_ryan_message(member):
-  messages = [
-    f"{member.mention} The only respectable thing about you, old sport, is your money.",
-    f"{member.mention} Did my heart love 'til now? Forswear its sight. For I never saw true beauty 'til this night.",
-    f"{member.mention} Be Thankful for the noobs, for they have made you.",
-    f"{member.mention} had my curiosity... but now he has my attention.",
-    f"{member.mention} Give me my sin again.",
-    f"{member.mention} I’m having a birthday party but you’re not invited, but you can come if you want.",
-    f"{member.mention} Is it possible to improve on perfection?",
-    f"{member.mention} 'Tis an honor, Lord Ryab XVIII (the coolest one)"
-  ]
-  return random.choice(messages)
-
-def random_shhourti_message(member):
-  messages = [
-    f"{member.mention} Shhourti's like a melody in my head that I can't keep out.",
-    f"{member.mention} You got them Apple Bottom jeans and boots with the fur?",
-    f"{member.mention} Somebody call 9-1-1, Shhourti fire burning on the dance floor!",
-    f"{member.mention} I should be playing in the winter snow, but I’mma be under the mistletoe with Shhourti."
-  ]
-  return random.choice(messages)
-
-def random_jules_message(member):
-  messages = [
-    f"{member.mention} Bang 💎",
-    f"{member.mention} Hello NOT real Bwawan",
-    f"{member.mention} Oh look guys, is a noob betrayer 😒",
-    f"Eject {member.mention}, she's the the imposter!",
-    f"{member.mention} انا احب الجميع لكنك",
-    f"{member.mention} You ROCK! 🪨 Hah. Get it? Cause jewels are like... nevermind."
-  ]
-  return random.choice(messages)
-
-def random_matcha_message(member):
-  messages = [
-    f"{member.mention} Ew what even!? 🍵",
-    f"{member.mention} coffee is WAY better ☕",
-    f"{member.mention} so are you like, liquid pot or something?",
-    f"{member.mention} what's the latest Tic-Tac trend these days?"
-  ]
-  return random.choice(messages)
-
 def random_deuces_message(member):
   messages = [
     f"{member.mention} Hey Daddy 😘"
@@ -84,19 +36,89 @@ def random_bub_message():
   ]
   return random.choice(messages)
 
+def make_bub_messages(member):
+  return [
+    f'{member.mention} игнорирование'
+  ]
+
+def make_ssmonk_messages(member):
+  return [
+    f'{member.mention} Hey sexy 😉'
+  ]
+
+def make_bub_messages(member):
+  return [
+    f'{member.mention} игнорирование'
+  ]
+
+def make_deuces_messages(member):
+  return [
+    f"{member.mention} Hey Daddy 😘"
+  ]
+
+def make_matcha_messages(member):
+  return [
+    f"{member.mention} Ew what even!? 🍵",
+    f"{member.mention} coffee is WAY better ☕",
+    f"{member.mention} so are you like, liquid pot or something?",
+    f"{member.mention} what's the latest Tic-Tac trend these days?"
+  ]
+
+def make_jules_messages(member):
+  return [
+    f"{member.mention} Bang 💎",
+    f"{member.mention} Hello NOT real Bwawan",
+    f"{member.mention} Oh look guys, is a noob betrayer 😒",
+    f"Eject {member.mention}, she's the the imposter!",
+    f"{member.mention} انا احب الجميع لكنك",
+    f"{member.mention} You ROCK! 🪨 Hah. Get it? Cause jewels are like... nevermind."
+  ]
+
+def make_brit_messages(member):
+  return [
+    f"{member.mention} Shhourti's like a melody in my head that I can't keep out.",
+    f"{member.mention} You got them Apple Bottom jeans and boots with the fur?",
+    f"{member.mention} Somebody call 9-1-1, Shhourti fire burning on the dance floor!",
+    f"{member.mention} I should be playing in the winter snow, but I’mma be under the mistletoe with Shhourti."
+  ]
+
+def make_ryan_messages(member):
+  return [
+    f"{member.mention} The only respectable thing about you, old sport, is your money.",
+    f"{member.mention} Did my heart love 'til now? Forswear its sight. For I never saw true beauty 'til this night.",
+    f"{member.mention} Be Thankful for the noobs, for they have made you.",
+    f"{member.mention} had my curiosity... but now he has my attention.",
+    f"{member.mention} Give me my sin again.",
+    f"{member.mention} I’m having a birthday party but you’re not invited, but you can come if you want.",
+    f"{member.mention} Is it possible to improve on perfection?",
+    f"{member.mention} 'Tis an honor, Lord Ryab XVIII (the coolest one)"
+  ]
+
+def messages_for(member):
+  message_fns = {
+    'A_ssmonk#0763': make_ssmonk_messages,
+    '¯\\_(ツ)_/¯#3216': make_bub_messages,
+    'ᴰᵉᵘᶜᵉˢ#9928': make_deuces_messages,
+    'jazfunk#6114': make_matcha_messages,
+    'jules#3901': make_jules_messages,
+    'shhourti#6292': make_brit_messages,
+    'Hira#2710': make_ryan_messages
+  }
+
+  id = f'{member.name}#{member.discriminator}'
+  if id in message_fns:
+    return message_fns[id](member)
+
 async def greet_member(member, channel):
-  if member.name == 'ᴰᵉᵘᶜᵉˢ' and member.discriminator == '9928':
-    await channel.send(random_deuces_message(member))
-  elif member.name == 'jazfunk' and member.discriminator == '6114':
-    await channel.send(random_matcha_message(member))
-  elif member.name == 'jules' and member.discriminator == '3901':
-    await channel.send(random_jules_message(member))
-  elif member.name == 'Hira' and member.discriminator == '2710':
-    await channel.send(random_ryan_message(member))
-  elif member.name == 'shhourti' and member.discriminator == '6292':
-    await channel.send(random_shhourti_message(member))
-  elif member.name == 'A_ssmonk' and member.discriminator == '0763':
-    await channel.send(random_ssmonk_message(member))
+  messages = messages_for(member)
+  if messages:
+    await channel.send(random.choice(messages))
+
+def has_joined(before, after):
+  return before.status == discord.Status.offline and after.status != discord.Status.offline
+
+def can_send_message(member, channel):
+  return channel.permissions_for(member).send_messages
 
 def should_greet(member, channel):
   r4_url = 'https://discord.com/channels/976337221663723521/1000437303870750801'
@@ -109,10 +131,10 @@ async def on_presence_update(before, after):
       if should_greet(after, channel):
         await greet_member(after, channel)
 
-client.run(TOKEN)
-
 async def exit_gracefully(self, *args):
   await client.close()
 
-signal.signal(signal.SIGINT, exit_gracefully)
-signal.signal(signal.SIGTERM, exit_gracefully)
+if __name__ == '__main__':
+  client.run(TOKEN)
+  signal.signal(signal.SIGINT, exit_gracefully)
+  signal.signal(signal.SIGTERM, exit_gracefully)
---
+++ dicaprio/bot_test.py
@@ -0,0 +1,128 @@
+import unittest
+import asynctest
+import random
+from bot import messages_for, greet_member
+
+class Member:
+  def __init__(self, name, discriminator, mention):
+    self.name = name
+    self.discriminator = discriminator
+    self.mention = mention
+
+class Channel:
+  def __init__(self):
+    self.sent_message = False
+    self.message = None
+
+  async def send(self, message):
+    self.sent_message = True
+    self.message = message
+
+fake_user = Member('garbage', '0123', 'Imposter!!!')
+bub = Member('¯\_(ツ)_/¯', '3216', 'Bub')
+ssmonk = Member('A_ssmonk', '0763', 'ssmonk')
+deuces = Member('ᴰᵉᵘᶜᵉˢ', '9928', 'Papi')
+matcha = Member('jazfunk', '6114', 'matcha')
+jules = Member('jules', '3901', 'Kok')
+brit = Member('shhourti', '6292', 'Brit')
+ryan = Member('Hira', '2710', 'Ryan')
+
+
+class TestBotMethods(unittest.TestCase):
+
+  def test_messages_for_none(self):
+    messages = messages_for(fake_user)
+    self.assertEqual(True, not messages)
+
+  def test_messages_for_bub(self):
+    messages = messages_for(bub)
+    expected_messages = ['Bub игнорирование']
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_ssmonk(self):
+    messages = messages_for(ssmonk)
+    expected_messages = ['ssmonk Hey sexy 😉']
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_ssmonk(self):
+    messages = messages_for(ssmonk)
+    expected_messages = ['ssmonk Hey sexy 😉']
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_deuces(self):
+    messages = messages_for(deuces)
+    expected_messages = ['Papi Hey Daddy 😘']
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_matcha(self):
+    messages = messages_for(matcha)
+    expected_messages = [
+      "matcha Ew what even!? 🍵",
+      "matcha coffee is WAY better ☕",
+      "matcha so are you like, liquid pot or something?",
+      "matcha what's the latest Tic-Tac trend these days?"
+    ]
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_jules(self):
+    messages = messages_for(jules)
+    expected_messages = [
+      "Kok Bang 💎",
+      "Kok Hello NOT real Bwawan",
+      "Kok Oh look guys, is a noob betrayer 😒",
+      "Eject Kok, she's the the imposter!",
+      "Kok انا احب الجميع لكنك",
+      "Kok You ROCK! 🪨 Hah. Get it? Cause jewels are like... nevermind."
+    ]
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_brit(self):
+    messages = messages_for(brit)
+    expected_messages = [
+      "Brit Shhourti's like a melody in my head that I can't keep out.",
+      "Brit You got them Apple Bottom jeans and boots with the fur?",
+      "Brit Somebody call 9-1-1, Shhourti fire burning on the dance floor!",
+      "Brit I should be playing in the winter snow, but I’mma be under the mistletoe with Shhourti."
+    ]
+    self.assertEqual(messages, expected_messages)
+
+  def test_messages_for_ryan(self):
+    messages = messages_for(ryan)
+    expected_messages = [
+      "Ryan The only respectable thing about you, old sport, is your money.",
+      "Ryan Did my heart love 'til now? Forswear its sight. For I never saw true beauty 'til this night.",
+      "Ryan Be Thankful for the noobs, for they have made you.",
+      "Ryan had my curiosity... but now he has my attention.",
+      "Ryan Give me my sin again.",
+      "Ryan I’m having a birthday party but you’re not invited, but you can come if you want.",
+      "Ryan Is it possible to improve on perfection?",
+      "Ryan 'Tis an honor, Lord Ryab XVIII (the coolest one)"
+    ]
+    self.assertEqual(messages, expected_messages)
+
+
+class TestAsyncBotMethods(asynctest.TestCase):
+  async def test_greet_nobody(self):
+    channel = Channel()
+    await greet_member(fake_user, channel)
+    self.assertEqual(False, channel.sent_message)
+
+  async def test_greet_ryan(self):
+    channel = Channel()
+    ryan_messages = [
+      "Ryan The only respectable thing about you, old sport, is your money.",
+      "Ryan Did my heart love 'til now? Forswear its sight. For I never saw true beauty 'til this night.",
+      "Ryan Be Thankful for the noobs, for they have made you.",
+      "Ryan had my curiosity... but now he has my attention.",
+      "Ryan Give me my sin again.",
+      "Ryan I’m having a birthday party but you’re not invited, but you can come if you want.",
+      "Ryan Is it possible to improve on perfection?",
+      "Ryan 'Tis an honor, Lord Ryab XVIII (the coolest one)"
+    ]
+    await greet_member(ryan, channel)
+    self.assertEqual(True, channel.sent_message)
+    self.assertEqual(True, channel.message in ryan_messages)
+
+
+if __name__ == '__main__':
+  unittest.main()
